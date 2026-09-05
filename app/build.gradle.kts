25bbe584032ea71ad48ce49826c9393ac629fae9
mwilliams455 <m.williams.455@gmail.com>
2026-09-05 13:12:53 +0100

    android: add diagnostic app module

---
+++ app/build.gradle.kts
@@ -0,0 +1,33 @@
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "com.m10r.diagnostic"
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "com.m10r.diagnostic"
+        minSdk = 26
+        targetSdk = 36
+        versionCode = 1
+        versionName = "0.1.0-diagnostic"
+
+        testInstrumentationRunner = "android.test.InstrumentationTestRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+}
+
+dependencies {
+    testImplementation("junit:junit:4.13.2")
+}
