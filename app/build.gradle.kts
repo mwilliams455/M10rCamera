95d6fb6031e60ec70fc27878d5f99b63e13a2b5b
mwilliams455 <m.williams.455@gmail.com>
2026-09-05 17:27:40 +0100

    v0.5.2 mark highlight A/B build identity

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.m10r.diagnostic"
         minSdk = 26
         targetSdk = 36
-        versionCode = 6
-        versionName = "0.5.1-leica-linear-headroom"
+        versionCode = 7
+        versionName = "0.5.2-highlight-ab"
 
         testInstrumentationRunner = "android.test.InstrumentationTestRunner"
     }
